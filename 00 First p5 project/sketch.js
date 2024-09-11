8d58a8094cd23934331272926ab499767aebb32a
stefan-scott <scottst@spsd.sk.ca>
2024-09-11 13:01:41 -0600

    done

---
+++ 00 First p5 project/sketch.js
@@ -0,0 +1,16 @@
+// First Day Demo
+// Mr. Scott
+// Sept 10, 2024
+
+// Interactive Programs -> don't finish...
+
+function setup() {
+  //Happens one time, at the start
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  // Happens over and over, targeting 60fps
+  //background(220);
+  circle(mouseX, mouseY, 20);
+}

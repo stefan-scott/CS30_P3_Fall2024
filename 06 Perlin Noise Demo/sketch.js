3fb2179feaf93251c84d58e63f1eea54b2b4bafb
stefan-scott <scottst@spsd.sk.ca>
2024-10-01 14:18:24 -0600

    finish demo

--- 06 Perlin Noise Demo/sketch.js
+++ 06 Perlin Noise Demo/sketch.js
@@ -3,10 +3,12 @@
 // Sept 27, 2024
 
 // noise() vs random()
+let circleTime = 5;
+let circleInterval = 0.05; //amount to push clock fwd
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  frameRate(2);
+  frameRate(15);
 }
 
 function draw() {
@@ -15,12 +17,41 @@ function draw() {
   //random()
   randomCircle();
   //noise()
+  noiseCircle();
+  staircase();
 }
+
+function staircase(){
+  //use a for loop to draw a series of 
+  //rectangles for use as terrain
+  randomSeed(1); //only for random()
+  let rectWidth = 5;
+  for(let x = 0; x <= width; x += rectWidth){
+    noFill();
+    //generate a random height for each rect
+    let rectHeight = random(50,500);
+    rect(x, height, rectWidth, -rectHeight);
+  }
+}
+
+
+function noiseCircle(){
+  // draw a circle on the screen using
+  // noise() for random values (diameter, then color)
+  let cSize = noise(circleTime);  //0-1
+  cSize = map(cSize, 0, 1, 10, 255);
+  fill(cSize, cSize/2, 255-cSize);
+  circle(width*0.75, height/2, cSize);
+  text(round(cSize), width * 0.75, height/2);
+  circleTime += circleInterval;
+}
+
 
 function randomCircle(){
   //draw a circle on the screen, using 
   //random values for the diameter  (later, color)
-  let cSize = random(10,200);
+  let cSize = random(10,255);
+  fill(cSize, cSize/2, 255-cSize);
   circle(width*0.25, height/2, cSize);
   textAlign(CENTER, CENTER);
   textSize(30);

d45c1483dd9b92ee4e40121686b8d77a0c647610
stefan-scott <scottst@spsd.sk.ca>
2025-01-15 11:54:03 -0600

    wip

---
+++ 14 Final Coding Practice/sketch.js
@@ -0,0 +1,59 @@
+// Final Coding Practice
+// Jan 15, 2025
+// Mr. Scott
+
+let gorillaIdle = [];
+let gorillaSwipe = [];
+
+let gorillaState = 0;   //0-Idle   1-Swipe
+let idleIndex = 0; let swipeIndex = 0;
+
+let spirals = [];  //for objects later on...
+
+function preload() {
+  //Gorilla Images
+  for (let i = 1; i <= 6; i++) {
+    gorillaIdle.push(loadImage("assets/Gorilla/idle" + i + ".png"))
+    gorillaSwipe.push(loadImage("assets/Gorilla/swipe" + i + ".png"))
+  }
+
+  //Spiral Images
+  for (let i = 0; i <= 15; i++) {
+    if (i < 10) {
+      spirals.push(loadImage("assets/Circle/Circle Animation0" + i + ".png"));
+    }
+    else {
+      spirals.push(loadImage("assets/Circle/Circle Animation" + i + ".png"));
+    }
+  }
+}
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+}
+
+function draw() {
+  background(220);
+
+  //Gorilla Code
+  if (gorillaState === 0) { //IDLE
+    image(gorillaIdle[idleIndex], width / 2, height / 2);
+    if (frameCount % 10 === 0) {
+      idleIndex++;
+      if (idleIndex > 5) {
+        idleIndex = 0;
+      }
+    }
+  }
+  else if (gorillaState === 1) { //SWIPE
+    image(gorillaSwipe[swipeIndex], width / 2, height / 2);
+    if (frameCount % 10 === 0) {
+      swipeIndex++;
+      if (swipeIndex > 5) {
+        swipeIndex = 0;
+      }
+    }
+  }
+}
